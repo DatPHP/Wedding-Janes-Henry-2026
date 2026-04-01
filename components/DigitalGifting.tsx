63644be213df9970a1992e02b2d4be2c21cfd6f8
DatPHP <nguyenvandat170296@gmail.com>
2026-04-01 08:21:14 +0800

    okki

--- components/DigitalGifting.tsx
+++ components/DigitalGifting.tsx
@@ -59,7 +59,7 @@ export default function DigitalGifting() {
                                         <img src="/images/QR_banking_janes.jpg" alt="Janes's QR Code" />
                                     </div>
                                     <p className="font-semibold text-foreground">Janes & Henry</p>
-                                    <p className="text-sm text-muted">Bank Name: ACB</p>
+                                    <p className="text-sm text-muted">Bank Name: Asia Commercial Bank (ACB)</p>
                                     <p className="text-sm text-muted">Account: 4562587</p>
                                 </div>
                             </div>
